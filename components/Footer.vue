fd080953251be0612dbd04124733e74d66b4eeaa
pumin <pumin@pumins-MacBook-Pro.local>
2019-12-25 15:34:31 +0700

    interstellar project

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-layout id="components-layout-demo-fixed">
+    <a-layout-footer
+      :style="{ textAlign: 'center',background:'#001529', color:'#fff' }"
+    >Interstellar Design ©2020 Created by Pumin</a-layout-footer>
+  </a-layout>
+</template>
+
+
+<script>
+import Header from "~/components/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+/*  */
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-layout id="components-layout-demo-fixed">
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :defaultSelectedKeys="['1']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">Star 1</a-menu-item>
+        <a-menu-item key="2">Star 2</a-menu-item>
+        <a-menu-item key="3">Star 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+  </a-layout>
+</template>
+
+
+<script>
+import Header from "~/components/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+#components-layout-demo-fixed .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
+}
+</style>
